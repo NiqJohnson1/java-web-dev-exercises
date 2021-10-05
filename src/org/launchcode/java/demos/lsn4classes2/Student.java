23b322b38dd59dfa6c38c8b008851d2556eb2638
Niquelle <niqjohnson1@gmail.com>
2021-10-05 18:51:46 -0400

    finished Restaurant Pt 2 studio

--- src/org/launchcode/java/demos/lsn4classes2/Student.java
+++ src/org/launchcode/java/demos/lsn4classes2/Student.java
@@ -1,105 +1,105 @@
-package org.launchcode.java.demos.lsn4classes2;
-
-public class Student {
-
-    private static int nextStudentId = 1;
-    private String name;
-    private int studentId;
-    private int numberOfCredits = 0;
-    private double gpa = 0.0;
-
-    public Student (String name, int studentId, int numberOfCredits, double gpa) {
-        this.name = name;
-        this.studentId = studentId;
-        this.numberOfCredits = numberOfCredits;
-        this.gpa = gpa;
-    }
-
-    public Student(String name, int studentId) {
-        this(name, studentId, 0, 0);
-    }
-
-    public Student(String name) {
-        this(name, nextStudentId);
-        nextStudentId++;
-    }
-
-    public String studentInfo() {
-        return (this.name + " has a GPA of: " + this.gpa);
-    }
-
-//    Freshman (0-29 credits), Sophomore (30-59 credits), Junior (60-89 credits), or Senior (90+ credits).
-     //TODO: Uncomment and complete the getGradeLevel method here:
-    public static String getGradeLevel(int numberOfCredits) {
-        // Determine the grade level of the student based on numberOfCredits
-        if(numberOfCredits <= 29) {
-            return ("Student is a Freshman");
-        }
-        if(numberOfCredits <= 59 ) {
-            return ("Student is a Sophomore");
-        }
-        if(numberOfCredits <= 89) {
-            return ("Student is a Junior");
-        }
-        else{
-            return ("Student is a Senior");
-        }
-    }
-
-    // TODO: Complete the addGrade method.
-    public void addGrade(int courseCredits, double grade) {
-        int totalGrade = 0;
-        int numberOfCredits = courseCredits;
-
-        }
-        // Update the appropriate fields: numberOfCredits, gpa
-    }
-
-    // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather
-    //  than just the class fields.
-
-    // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
-    //  Student objects equal.
-
-    public String getName() {
-        return name;
-    }
-
-    public int getStudentId() {
-        return studentId;
-    }
-
-    public int getNumberOfCredits() {
-        return numberOfCredits;
-    }
-
-    public double getGpa() {
-        return gpa;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setStudentId(int studentId) {
-        this.studentId = studentId;
-    }
-
-    public void setGpa(double gpa) {
-        this.gpa = gpa;
-    }
-
-    private void setNumberOfCredits(int numberOfCredits) {
-        this.numberOfCredits = numberOfCredits;
-    }
-
-    public static void main(String[] args) {
-        Student sally = new Student("Sally",1,1,4.0);
-        System.out.println("The Student class works! " + sally.getName() + " is a student!");
-        System.out.println(sally);
-        sally.addGrade(12, 3.5);
-        System.out.println(sally);
-        sally.addGrade(25, 3.8);
-        System.out.println(sally);
-    }
-}
+//package org.launchcode.java.demos.lsn4classes2;
+//
+//public class Student {
+//
+//    private static int nextStudentId = 1;
+//    private String name;
+//    private int studentId;
+//    private int numberOfCredits = 0;
+//    private double gpa = 0.0;
+//
+//    public Student (String name, int studentId, int numberOfCredits, double gpa) {
+//        this.name = name;
+//        this.studentId = studentId;
+//        this.numberOfCredits = numberOfCredits;
+//        this.gpa = gpa;
+//    }
+//
+//    public Student(String name, int studentId) {
+//        this(name, studentId, 0, 0);
+//    }
+//
+//    public Student(String name) {
+//        this(name, nextStudentId);
+//        nextStudentId++;
+//    }
+//
+//    public String studentInfo() {
+//        return (this.name + " has a GPA of: " + this.gpa);
+//    }
+//
+////    Freshman (0-29 credits), Sophomore (30-59 credits), Junior (60-89 credits), or Senior (90+ credits).
+//     //TODO: Uncomment and complete the getGradeLevel method here:
+//    public static String getGradeLevel(int numberOfCredits) {
+//        // Determine the grade level of the student based on numberOfCredits
+//        if(numberOfCredits <= 29) {
+//            return ("Student is a Freshman");
+//        }
+//        if(numberOfCredits <= 59 ) {
+//            return ("Student is a Sophomore");
+//        }
+//        if(numberOfCredits <= 89) {
+//            return ("Student is a Junior");
+//        }
+//        else{
+//            return ("Student is a Senior");
+//        }
+//    }
+//
+//    // TODO: Complete the addGrade method.
+//    public void addGrade(int courseCredits, double grade) {
+//        int totalGrade = 0;
+//        int numberOfCredits = courseCredits;
+//
+//        }
+//        // Update the appropriate fields: numberOfCredits, gpa
+//    }
+//
+//    // TODO: Add your custom 'toString' method here. Make sure it returns a well-formatted String rather
+//    //  than just the class fields.
+//
+//    // TODO: Add your custom 'equals' method here. Consider which fields should match in order to call two
+//    //  Student objects equal.
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public int getStudentId() {
+//        return studentId;
+//    }
+//
+//    public int getNumberOfCredits() {
+//        return numberOfCredits;
+//    }
+//
+//    public double getGpa() {
+//        return gpa;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setStudentId(int studentId) {
+//        this.studentId = studentId;
+//    }
+//
+//    public void setGpa(double gpa) {
+//        this.gpa = gpa;
+//    }
+//
+//    private void setNumberOfCredits(int numberOfCredits) {
+//        this.numberOfCredits = numberOfCredits;
+//    }
+//
+//    public static void main(String[] args) {
+//        Student sally = new Student("Sally",1,1,4.0);
+//        System.out.println("The Student class works! " + sally.getName() + " is a student!");
+//        System.out.println(sally);
+//        sally.addGrade(12, 3.5);
+//        System.out.println(sally);
+//        sally.addGrade(25, 3.8);
+//        System.out.println(sally);
+//    }
+//}
--- src/org/launchcode/java/studio/restaurant/Restaurant.java
+++ src/org/launchcode/java/studio/restaurant/Restaurant.java
@@ -1,7 +1,40 @@
 package org.launchcode.java.studio.restaurant;
 
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Restaurant {
+
+    enum MenuCategory{
+        APPETIZER, MAIN_COURSE, DESSERT;
+    }
     public static void main(String[] args) {
-      Menu restaurantMenu = new Menu ();
+        MenuCategory category = MenuCategory.DESSERT;
+        Date updatedMenuDate = new Date();
+        ArrayList item = new ArrayList();
+      Menu restaurantMenu = new Menu (updatedMenuDate, item);
+      item.add(0,"Beef Dinner");
+        item.add(1,"Chicken Marsala");
+        item.add(2,"Baked Lemon Pepper Fish");
+
+        item.add(3,"Tiramisu");
+        item.add(4,"Peach Cobbler");
+        item.add(5,"Chocolate Molten Lava Cake");
+
+
+
+        System.out.println(MenuCategory.MAIN_COURSE);
+        System.out.println(item.get(0));
+        System.out.println(item.get(1));
+        System.out.println(item.get(2) + "\n");
+
+        System.out.println(MenuCategory.DESSERT);
+        System.out.println(item.get(3));
+        System.out.println(item.get(4));
+        System.out.println(item.get(5));
+
+        System.out.println("Menu Last Updated: " + updatedMenuDate);
+
     }
 }
